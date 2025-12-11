b2a8807371a49cc870fb7861dbef0e7765942916
MartynovMax <m555ax@icloud.com>
2025-12-11 21:12:26 +0100

    Implement fixed-width integer typedefs
    Refs #4

---
+++ core/base/core_types.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace core
+{
+    //--------------------------------------------------------------------------
+    // Integer types
+    //--------------------------------------------------------------------------
+
+    /// Signed 8-bit integer.
+    using i8 = signed char;
+    /// Unsigned 8-bit integer.
+    using u8 = unsigned char;
+
+    /// Signed 16-bit integer.
+    using i16 = short;
+    /// Unsigned 16-bit integer.
+    using u16 = unsigned short;
+
+    /// Signed 32-bit integer.
+    using i32 = int;
+    /// Unsigned 32-bit integer.
+    using u32 = unsigned int;
+
+    /// Signed 64-bit integer.
+    using i64 = long long;
+    /// Unsigned 64-bit integer.
+    using u64 = unsigned long long;
+}
