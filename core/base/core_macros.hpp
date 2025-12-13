d45b2f0ebcce099fa0c4e3281d6689a8a067d066
MartynovMax <m555ax@icloud.com>
2025-12-13 16:39:11 +0100

    Implement CORE_ARRAY_SIZE
    Refs #5

--- core/base/core_macros.hpp
+++ core/base/core_macros.hpp
@@ -12,3 +12,10 @@
 
 #define CORE_CONCAT_IMPL(a, b) a##b
 #define CORE_CONCAT(a, b) CORE_CONCAT_IMPL(a, b)
+
+// -----------------------------------------------------------------------------
+// Array size
+// NOTE: Intentionally unsafe for pointers (will "work" incorrectly by design).
+// -----------------------------------------------------------------------------
+
+#define CORE_ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
