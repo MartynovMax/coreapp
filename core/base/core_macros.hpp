563caea5a7c3a466615323609c379bf7b1ec8c13
MartynovMax <m555ax@icloud.com>
2025-12-13 16:37:03 +0100

    Implement stringification and concatenation
    Refs #5

--- core/base/core_macros.hpp
+++ core/base/core_macros.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+// -----------------------------------------------------------------------------
+// Stringification and concatenation
+// -----------------------------------------------------------------------------
+
+#define CORE_STRINGIFY_IMPL(x) #x
+#define CORE_STRINGIFY(x) CORE_STRINGIFY_IMPL(x)
+
+// Expands x first, then stringifies.
+#define CORE_STRINGIFY_VALUE(x) CORE_STRINGIFY_IMPL(x)
+
+#define CORE_CONCAT_IMPL(a, b) a##b
+#define CORE_CONCAT(a, b) CORE_CONCAT_IMPL(a, b)
