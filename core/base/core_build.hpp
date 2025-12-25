8e81c0a2f171d4b1d4adff38a997695ebe5ed86c
MartynovMax <m555ax@icloud.com>
2025-12-25 21:50:18 +0100

    Implement normalized build type macros
    Refs #10

--- core/base/core_build.hpp
+++ core/base/core_build.hpp
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "core_config.hpp"
+#include "core_macros.hpp"
+
+// -----------------------------------------------------------------------------
+// 1) Build type detection
+// -----------------------------------------------------------------------------
+
+#ifndef CORE_BUILD_DEBUG
+#define CORE_BUILD_DEBUG 0
+#endif
+
+#ifndef CORE_BUILD_RELEASE
+#define CORE_BUILD_RELEASE 0
+#endif
+
+#ifndef CORE_BUILD_DEVELOPMENT
+#define CORE_BUILD_DEVELOPMENT 0
+#endif
+
+#ifndef CORE_BUILD_SHIPPING
+#define CORE_BUILD_SHIPPING 0
+#endif
+
+#if (CORE_BUILD_DEBUG + CORE_BUILD_RELEASE + CORE_BUILD_DEVELOPMENT + CORE_BUILD_SHIPPING) == 0
+#if CORE_DEBUG
+#undef CORE_BUILD_DEBUG
+#define CORE_BUILD_DEBUG 1
+#else
+#undef CORE_BUILD_RELEASE
+#define CORE_BUILD_RELEASE 1
+#endif
+#endif
+
+#if (CORE_BUILD_DEBUG + CORE_BUILD_RELEASE + CORE_BUILD_DEVELOPMENT + CORE_BUILD_SHIPPING) != 1
+#error "Build-type detection error: expected exactly one CORE_BUILD_* == 1."
+#endif
