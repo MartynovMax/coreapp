c8e27378ea61154f7a10f161ca9b5d2e6770fe1a
MartynovMax <m555ax@icloud.com>
2025-12-28 12:58:46 +0100

    Implement CORE_CPP_VERSION and CORE_CPPxx_OR_GREATER helpers
    Refs #11

--- core/base/core_preprocessor.hpp
+++ core/base/core_preprocessor.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "core/base/core_platform.hpp"
+
+// -----------------------------------------------------------------------------
+// C++ standard version helpers
+// -----------------------------------------------------------------------------
+
+#if !defined(CORE_CPP_VERSION)
+#if CORE_COMPILER_MSVC && !defined(__clang__)
+#define CORE_CPP_VERSION _MSVC_LANG
+#else
+#define CORE_CPP_VERSION __cplusplus
+#endif
+#endif
+
+#define CORE_CPP11_OR_GREATER (CORE_CPP_VERSION >= 201103L)
+#define CORE_CPP14_OR_GREATER (CORE_CPP_VERSION >= 201402L)
+#define CORE_CPP17_OR_GREATER (CORE_CPP_VERSION >= 201703L)
+#define CORE_CPP20_OR_GREATER (CORE_CPP_VERSION >= 202002L)
