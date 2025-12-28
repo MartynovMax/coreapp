369d215ee6d93eeacd32388f7ce0c71fe2f02054
MartynovMax <m555ax@icloud.com>
2025-12-28 13:01:28 +0100

    Implement CORE_HAS_CPP_ATTRIBUTE and derived attribute flags
    Refs #11

--- core/base/core_preprocessor.hpp
+++ core/base/core_preprocessor.hpp
@@ -18,3 +18,31 @@
 #define CORE_CPP14_OR_GREATER (CORE_CPP_VERSION >= 201402L)
 #define CORE_CPP17_OR_GREATER (CORE_CPP_VERSION >= 201703L)
 #define CORE_CPP20_OR_GREATER (CORE_CPP_VERSION >= 202002L)
+
+// -----------------------------------------------------------------------------
+// Attribute detection
+// -----------------------------------------------------------------------------
+
+#if !defined(CORE_HAS_CPP_ATTRIBUTE)
+#if defined(__has_cpp_attribute)
+#define CORE_HAS_CPP_ATTRIBUTE(x) __has_cpp_attribute(x)
+#else
+#define CORE_HAS_CPP_ATTRIBUTE(x) 0
+#endif
+#endif
+
+#if !defined(CORE_HAS_NODISCARD)
+#define CORE_HAS_NODISCARD ((CORE_HAS_CPP_ATTRIBUTE(nodiscard) != 0) || CORE_CPP17_OR_GREATER)
+#endif
+
+#if !defined(CORE_HAS_NORETURN)
+#define CORE_HAS_NORETURN ((CORE_HAS_CPP_ATTRIBUTE(noreturn) != 0) || CORE_CPP11_OR_GREATER)
+#endif
+
+#if !defined(CORE_HAS_FALLTHROUGH)
+#define CORE_HAS_FALLTHROUGH ((CORE_HAS_CPP_ATTRIBUTE(fallthrough) != 0) || CORE_CPP17_OR_GREATER)
+#endif
+
+#if !defined(CORE_HAS_DEPRECATED)
+#define CORE_HAS_DEPRECATED ((CORE_HAS_CPP_ATTRIBUTE(deprecated) != 0) || CORE_CPP14_OR_GREATER)
+#endif
