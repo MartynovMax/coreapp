3bb5ebcd226e6af16fae870f199707501a0e0f5f
MartynovMax <m555ax@icloud.com>
2025-12-28 16:35:00 +0100

    Implement CORE_NO_INLINE with compiler-specific mappings and a safe fallback
    Refs #13

--- core/base/core_inline.hpp
+++ core/base/core_inline.hpp
@@ -27,3 +27,17 @@
 #define CORE_FORCE_INLINE inline
 #endif
 #endif
+
+// -----------------------------------------------------------------------------
+// No inline
+// -----------------------------------------------------------------------------
+
+#if !defined(CORE_NO_INLINE)
+#if CORE_COMPILER_MSVC
+#define CORE_NO_INLINE __declspec(noinline)
+#elif CORE_COMPILER_CLANG || CORE_COMPILER_GCC
+#define CORE_NO_INLINE __attribute__((noinline))
+#else
+#define CORE_NO_INLINE
+#endif
+#endif
