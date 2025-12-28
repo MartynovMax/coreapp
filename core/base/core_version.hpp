a8a5cec5fdb038ed2de4b0b50c02241ef7f4c0ca
MartynovMax <m555ax@icloud.com>
2025-12-28 21:50:43 +0100

    Define CORE_VERSION_MAJOR, CORE_VERSION_MINOR, CORE_VERSION_PATCH
    Refs #17

--- core/base/core_version.hpp
+++ core/base/core_version.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+// -----------------------------------------------------------------------------
+// Version components
+// -----------------------------------------------------------------------------
+#ifndef CORE_VERSION_MAJOR
+#define CORE_VERSION_MAJOR 0
+#endif
+
+#ifndef CORE_VERSION_MINOR
+#define CORE_VERSION_MINOR 1
+#endif
+
+#ifndef CORE_VERSION_PATCH
+#define CORE_VERSION_PATCH 0
+#endif
