002b0c2b3dfdb1eccea4004faaf73f1c8f490f28
MartynovMax <m555ax@icloud.com>
2025-12-28 22:31:38 +0100

    Add documentation for core_forward
    Refs #18

--- core/base/core_forward.hpp
+++ core/base/core_forward.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+// ============================================================================
+// core_forward.hpp
+// Centralized forward declarations for Core.
+// ============================================================================
+
+
 #ifndef CORE_NAMESPACE_BEGIN
 #define CORE_NAMESPACE_BEGIN namespace core {
 #endif
