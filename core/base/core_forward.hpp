2d5974bde9a56840b0fce18e8fcd58e7174a4655
MartynovMax <m555ax@icloud.com>
2025-12-28 22:30:03 +0100

    Define namespace helpers
    Refs #18

--- core/base/core_forward.hpp
+++ core/base/core_forward.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+#ifndef CORE_NAMESPACE_BEGIN
+#define CORE_NAMESPACE_BEGIN namespace core {
+#endif
+
+#ifndef CORE_NAMESPACE_END
+#define CORE_NAMESPACE_END }
+#endif
+
+CORE_NAMESPACE_BEGIN
+
+// (intentionally empty)
+
+CORE_NAMESPACE_END
