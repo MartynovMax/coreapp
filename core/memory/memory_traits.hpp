b90facbf1e3151182faa613c07e814d35e72e77e
MartynovMax <m555ax@icloud.com>
2026-01-02 20:45:49 +0100

    Add static assertions and simple usage examples in comments
    Refs #20

--- core/memory/memory_traits.hpp
+++ core/memory/memory_traits.hpp
@@ -132,28 +132,36 @@ struct is_trivially_copyable {
     static constexpr bool value = detail::IsTriviallyCopyableImpl<T>::value;
 };
 template <class T>
-inline constexpr bool is_trivially_copyable_v = is_trivially_copyable<T>::value;
+constexpr bool is_trivially_copyable_v() noexcept {
+    return is_trivially_copyable<T>::value;
+}
 
 template <class T>
 struct is_trivially_relocatable {
-    static constexpr bool value = is_trivially_copyable_v<T>;
+    static constexpr bool value = is_trivially_copyable_v<T>();
 };
 template <class T>
-inline constexpr bool is_trivially_relocatable_v = is_trivially_relocatable<T>::value;
+constexpr bool is_trivially_relocatable_v() noexcept {
+    return is_trivially_relocatable<T>::value;
+}
 
 template <class T>
 struct is_bitwise_serializable {
-    static constexpr bool value = is_trivially_copyable_v<T>;
+    static constexpr bool value = is_trivially_copyable_v<T>();
 };
 template <class T>
-inline constexpr bool is_bitwise_serializable_v = is_bitwise_serializable<T>::value;
+constexpr bool is_bitwise_serializable_v() noexcept {
+    return is_bitwise_serializable<T>::value;
+}
 
 template <class T>
 struct max_align_for {
     static constexpr usize value = static_cast<usize>(alignof(T));
 };
 template <class T>
-inline constexpr usize max_align_for_v = max_align_for<T>::value;
+constexpr usize max_align_for_v() noexcept {
+    return max_align_for<T>::value;
+}
 
 // ----------------------------------------------------------------------------
 // Size utilities
@@ -201,6 +209,61 @@ CORE_FORCE_INLINE constexpr usize CacheLineSize() noexcept {
     return static_cast<usize>(64);
 #endif
 }
+
+// ----------------------------------------------------------------------------
+// Static assertions and validation
+// ----------------------------------------------------------------------------
+
+namespace detail {
+
+static_assert(IsPowerOfTwo(1), "1 is power of two");
+static_assert(IsPowerOfTwo(2), "2 is power of two");
+static_assert(IsPowerOfTwo(16), "16 is power of two");
+static_assert(!IsPowerOfTwo(0), "0 is not power of two");
+static_assert(!IsPowerOfTwo(3), "3 is not power of two");
+
+static_assert(AlignUp(0, 16) == 0, "AlignUp(0, 16) == 0");
+static_assert(AlignUp(1, 16) == 16, "AlignUp(1, 16) == 16");
+static_assert(AlignUp(16, 16) == 16, "AlignUp(16, 16) == 16");
+static_assert(AlignUp(17, 16) == 32, "AlignUp(17, 16) == 32");
+
+static_assert(AlignDown(0, 16) == 0, "AlignDown(0, 16) == 0");
+static_assert(AlignDown(15, 16) == 0, "AlignDown(15, 16) == 0");
+static_assert(AlignDown(16, 16) == 16, "AlignDown(16, 16) == 16");
+static_assert(AlignDown(31, 16) == 16, "AlignDown(31, 16) == 16");
+
+static_assert(IsAlignedValue(0, 16), "0 is aligned to 16");
+static_assert(IsAlignedValue(16, 16), "16 is aligned to 16");
+static_assert(!IsAlignedValue(17, 16), "17 is not aligned to 16");
+
+static_assert(PaddingFor(0, 16) == 0, "PaddingFor(0, 16) == 0");
+static_assert(PaddingFor(1, 16) == 15, "PaddingFor(1, 16) == 15");
+static_assert(PaddingFor(16, 16) == 0, "PaddingFor(16, 16) == 0");
+static_assert(PaddingFor(17, 16) == 15, "PaddingFor(17, 16) == 15");
+
+static_assert(UsizeMax() > 0, "UsizeMax must be positive");
+static_assert(SafeMulSize(0, 100) == 0, "SafeMulSize(0, 100) == 0");
+static_assert(SafeMulSize(10, 20) == 200, "SafeMulSize(10, 20) == 200");
+
+static_assert(BytesFor<u32>(0) == 0, "BytesFor<u32>(0) == 0");
+static_assert(BytesFor<u32>(1) == 4, "BytesFor<u32>(1) == 4");
+static_assert(BytesFor<u32>(10) == 40, "BytesFor<u32>(10) == 40");
+
+static_assert(StorageSizeFor<u64>(5) == 40, "StorageSizeFor<u64>(5) == 40");
+
+static_assert(DefaultAlignment() > 0, "DefaultAlignment must be positive");
+static_assert(IsPowerOfTwo(DefaultAlignment()), "DefaultAlignment must be power of two");
+static_assert(CacheLineSize() > 0, "CacheLineSize must be positive");
+static_assert(IsPowerOfTwo(CacheLineSize()), "CacheLineSize must be power of two");
+
+struct TrivialStruct { int x; float y; };
+static_assert(is_trivially_copyable_v<int>(), "int is trivially copyable");
+static_assert(is_trivially_copyable_v<TrivialStruct>(), "TrivialStruct is trivially copyable");
+static_assert(is_trivially_relocatable_v<int>(), "int is trivially relocatable");
+static_assert(max_align_for_v<int>() == alignof(int), "max_align_for<int> matches alignof");
+static_assert(max_align_for_v<f64>() == alignof(f64), "max_align_for<f64> matches alignof");
+
+} // namespace detail 
 
 } // namespace core
 
