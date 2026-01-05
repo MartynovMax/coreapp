f4373ed78860354b8b40fba287b68857485b8977
MartynovMax <m555ax@icloud.com>
2026-01-05 13:32:14 +0100

    Create core/memory/malloc_allocator.hpp
    Refs #22

---
+++ core/memory/malloc_allocator.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// =============================================================================
+// malloc_allocator.hpp
+// Simple allocator wrapping C runtime malloc/free.
+// =============================================================================
+
+#include "core_memory.hpp"
+#include "core_allocator.hpp"
+
+namespace core {
+
+} // namespace core
+
