394cc0d24ea43f9e506832c047caacdddd947503
MartynovMax <m555ax@icloud.com>
2026-01-07 20:44:18 +0100

    Implement atomic_uintptr type alias
    Refs #110

--- core/concurrency/atomic_uintptr.hpp
+++ core/concurrency/atomic_uintptr.hpp
@@ -9,10 +9,21 @@
 
 #include "../base/core_config.hpp"
 #include "../base/core_types.hpp"
-#include "atomic_intrinsics.hpp"
-#include "memory_order.hpp"
+#include "../base/core_platform.hpp"
+#include "atomic_u32.hpp"
+#if CORE_64BIT
+#include "atomic_u64.hpp"
+#endif
 
 namespace core {
+
+// atomic_uintptr is a type alias to atomic_u32 or atomic_u64
+// depending on pointer size.
+#if CORE_64BIT
+using atomic_uintptr = atomic_u64;
+#else
+using atomic_uintptr = atomic_u32;
+#endif
 
 } // namespace core
 
