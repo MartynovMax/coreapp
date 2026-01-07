706d5b5a704a8580092193ae309e3c6e38e8dbbb
MartynovMax <m555ax@icloud.com>
2026-01-07 20:40:15 +0100

    Create core/concurrency/atomic_uintptr.hpp
    Refs #110

---
+++ core/concurrency/atomic_uintptr.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+// =============================================================================
+// atomic_uintptr.hpp
+// Pointer-sized unsigned atomic integer type.
+//
+// Atomic integer with the same size as a pointer (usize).
+// =============================================================================
+
+#include "../base/core_config.hpp"
+#include "../base/core_types.hpp"
+#include "atomic_intrinsics.hpp"
+#include "memory_order.hpp"
+
+namespace core {
+
+} // namespace core
+
