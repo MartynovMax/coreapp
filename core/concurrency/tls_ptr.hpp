0e4a8903aa1408a51322444522d77559fe7b625b
MartynovMax <m555ax@icloud.com>
2026-01-07 23:44:51 +0100

    Add tls_ptr class declaration
    Refs #111

--- core/concurrency/tls_ptr.hpp
+++ core/concurrency/tls_ptr.hpp
@@ -13,6 +13,30 @@
 
 namespace core {
 
+// =============================================================================
+// tls_ptr<T> - Thread-local pointer storage
+// =============================================================================
+
+template<typename T>
+class tls_ptr {
+public:
+    tls_ptr() = delete;
+    tls_ptr(const tls_ptr&) = delete;
+    tls_ptr& operator=(const tls_ptr&) = delete;
+
+    /// Get current thread's pointer value.
+    CORE_NODISCARD static T* get() noexcept;
+
+    /// Set current thread's pointer value.
+    static void set(T* ptr) noexcept;
+
+private:
+#if CORE_HAS_THREADS && CORE_HAS_THREAD_LOCAL
+    static thread_local T* s_value;
+#else
+    static T* s_value;
+#endif
+};
 
 } // namespace core
 
