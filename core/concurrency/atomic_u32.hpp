46ceabc9508f0ae0621b564c7b235a0852013f4f
MartynovMax <m555ax@icloud.com>
2026-01-07 19:02:45 +0100

    Create core/concurrency/atomic_u32.hpp
    Refs #110

---
+++ core/concurrency/atomic_u32.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// =============================================================================
+// atomic_u32.hpp
+// 32-bit unsigned atomic integer type.
+// =============================================================================
+
+#include "../base/core_config.hpp"
+#include "../base/core_types.hpp"
+#include "atomic_intrinsics.hpp"
+#include "memory_order.hpp"
+
+namespace core {
+
+} // namespace core
+
