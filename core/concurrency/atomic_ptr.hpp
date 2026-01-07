c54ea7fe78cc418a2f3e52152ac58ef43d40cf3e
MartynovMax <m555ax@icloud.com>
2026-01-07 20:55:01 +0100

    Create core/concurrency/atomic_ptr.hpp
    Refs #110

---
+++ core/concurrency/atomic_ptr.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+// =============================================================================
+// atomic_ptr.hpp
+// Atomic pointer type.
+//
+// Type-safe atomic pointer operations for void*.
+// =============================================================================
+
+#include "../base/core_config.hpp"
+#include "../base/core_types.hpp"
+#include "atomic_intrinsics.hpp"
+#include "memory_order.hpp"
+
+namespace core {
+
+} // namespace core
+
