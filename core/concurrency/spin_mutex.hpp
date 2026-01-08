d96bd761fb9c237e69919fe5e69556cccdecbd24
MartynovMax <m555ax@icloud.com>
2026-01-08 09:34:57 +0100

    Implement spin_mutex::lock() with two-phase spinning and exponential backoff
    Refs #112

--- core/concurrency/spin_mutex.hpp
+++ core/concurrency/spin_mutex.hpp
@@ -11,6 +11,7 @@
 #include "../base/core_config.hpp"
 #include "../base/core_features.hpp"
 #include "../base/core_types.hpp"
+#include "../base/core_assert.hpp"
 #include "core_atomic.hpp"
 #include "cpu_relax.hpp"
 
@@ -40,6 +41,36 @@ private:
     #endif
 #endif
 };
+
+// -----------------------------------------------------------------------------
+// Implementation
+// -----------------------------------------------------------------------------
+
+inline void spin_mutex::lock() noexcept {
+#if CORE_HAS_THREADS
+    #if CORE_DEBUG
+    u32 prev_count = m_lock_count.fetch_add(1, memory_order::relaxed);
+    CORE_ASSERT(prev_count == 0, "Double lock detected");
+    #endif
+
+    u32 spin_count = 0;
+    constexpr u32 MAX_SPIN = 32;
+
+    while (m_flag.exchange(1, memory_order::acquire) != 0) {
+        spin_count = 0;
+        while (m_flag.load(memory_order::relaxed) != 0) {
+            if (spin_count < MAX_SPIN) {
+                for (u32 i = 0; i < (1u << spin_count); ++i) {
+                    cpu_relax();
+                }
+                ++spin_count;
+            } else {
+                cpu_relax();
+            }
+        }
+    }
+#endif
+}
 
 } // namespace core
 
