99681a194a6f72fe27cb94e4a6cbd584bb655dfc
MartynovMax <m555ax@icloud.com>
2026-01-09 13:08:57 +0100

    Create core/memory/memory_ops.hpp file with header structure
    Refs #45

---
+++ core/memory/memory_ops.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// =============================================================================
+// memory_ops.hpp
+// Core-level wrappers for basic memory operations.
+// =============================================================================
+
+#include "core_memory.hpp"
+
+namespace core {
+
+namespace detail {
+
+} // namespace detail
+
+} // namespace core
+
