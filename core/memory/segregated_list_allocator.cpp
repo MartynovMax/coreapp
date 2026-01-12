a101b571a2cb1093ab583fdcc26584128e60b964
MartynovMax <m555ax@icloud.com>
2026-01-12 22:34:18 +0100

    Define SegregatedListAllocator implementing IAllocator
    Refs #30

--- core/memory/segregated_list_allocator.cpp
+++ core/memory/segregated_list_allocator.cpp
@@ -2,6 +2,25 @@
 
 namespace core {
 
+SegregatedListAllocator::SegregatedListAllocator() noexcept {
+}
+
+SegregatedListAllocator::~SegregatedListAllocator() noexcept {
+}
+
+void* SegregatedListAllocator::Allocate(const AllocationRequest& request) noexcept {
+    CORE_UNUSED(request);
+    return nullptr;
+}
+
+void SegregatedListAllocator::Deallocate(const AllocationInfo& info) noexcept {
+    CORE_UNUSED(info);
+}
+
+bool SegregatedListAllocator::Owns(const void* ptr) const noexcept {
+    CORE_UNUSED(ptr);
+    return false;
+}
 
 } // namespace core
 
--- core/memory/segregated_list_allocator.hpp
+++ core/memory/segregated_list_allocator.hpp
@@ -11,6 +11,19 @@
 
 namespace core {
 
+class SegregatedListAllocator final : public IAllocator {
+public:
+    SegregatedListAllocator() noexcept;
+    ~SegregatedListAllocator() noexcept override;
+
+    SegregatedListAllocator(const SegregatedListAllocator&) = delete;
+    SegregatedListAllocator& operator=(const SegregatedListAllocator&) = delete;
+
+    // IAllocator interface
+    void* Allocate(const AllocationRequest& request) noexcept override;
+    void Deallocate(const AllocationInfo& info) noexcept override;
+    bool Owns(const void* ptr) const noexcept override;
+};
 
 } // namespace core
 
