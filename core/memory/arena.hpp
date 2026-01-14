10e2e508d7f1b4f745a482193676dbb4a0447a1b
MartynovMax <m555ax@icloud.com>
2026-01-14 22:06:41 +0100

    Define ArenaMarker type
    Refs #39

--- core/memory/arena.hpp
+++ core/memory/arena.hpp
@@ -10,5 +10,29 @@
 
 namespace core {
 
+// ----------------------------------------------------------------------------
+// ArenaMarker - Checkpoint for arena state
+// ----------------------------------------------------------------------------
+
+struct ArenaMarker {
+    void* internal_state = nullptr;
+    
+#if CORE_MEMORY_DEBUG
+    u32 debug_arena_id = 0;
+    u32 debug_sequence = 0;
+#endif
+    
+    bool operator==(const ArenaMarker& other) const noexcept {
+        return internal_state == other.internal_state;
+    }
+    
+    bool operator!=(const ArenaMarker& other) const noexcept {
+        return internal_state != other.internal_state;
+    }
+};
+
+static_assert(sizeof(ArenaMarker) <= 16, "ArenaMarker should be small");
+static_assert(is_trivially_copyable_v<ArenaMarker>(), "ArenaMarker must be trivially copyable");
+
 } // namespace core
 
