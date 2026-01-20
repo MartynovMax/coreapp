c41732268a7f4bb1538b1caa15782809b2dab0ee
MartynovMax <m555ax@icloud.com>
2026-01-20 22:58:34 +0100

    Create benchmarks/experiments/null_experiment.cpp with implementation
    Refs #122

---
+++ benchmarks/experiments/null_experiment.cpp
@@ -0,0 +1,40 @@
+#include "null_experiment.hpp"
+
+namespace core {
+namespace bench {
+
+void NullExperiment::Setup(const ExperimentParams& params) noexcept {
+    (void)params;
+}
+
+void NullExperiment::Warmup() noexcept {
+}
+
+void NullExperiment::RunPhases() noexcept {
+}
+
+void NullExperiment::Teardown() noexcept {
+}
+
+const char* NullExperiment::Name() const noexcept {
+    return "null";
+}
+
+const char* NullExperiment::Category() const noexcept {
+    return "test";
+}
+
+const char* NullExperiment::Description() const noexcept {
+    return "No-op experiment for testing runner";
+}
+
+const char* NullExperiment::AllocatorName() const noexcept {
+    return "none";
+}
+
+IExperiment* NullExperiment::Create() noexcept {
+    return new NullExperiment();
+}
+
+} // namespace bench
+} // namespace core
