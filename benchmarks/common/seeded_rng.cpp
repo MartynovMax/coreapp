b3b0769fae4f64008b59d02503725e6078e85515
MartynovMax <m555ax@icloud.com>
2026-01-20 22:46:04 +0100

    Create benchmarks/common/seeded_rng.cpp with deterministic RNG implementation (next_u32/next_u64/next_range)
    Refs #122

---
+++ benchmarks/common/seeded_rng.cpp
@@ -0,0 +1,36 @@
+#include "seeded_rng.hpp"
+
+namespace core {
+namespace bench {
+
+SeededRNG::SeededRNG(u64 seed) noexcept
+    : _state(seed)
+{
+    if (_state == 0) {
+        _state = 1;
+    }
+}
+
+u64 SeededRNG::NextU64() noexcept {
+    u64 x = _state;
+    x ^= x >> 12;
+    x ^= x << 25;
+    x ^= x >> 27;
+    _state = x;
+    return x * 0x2545F4914F6CDD1Dull;
+}
+
+u32 SeededRNG::NextU32() noexcept {
+    return static_cast<u32>(NextU64());
+}
+
+u32 SeededRNG::NextRange(u32 min, u32 max) noexcept {
+    if (min >= max) {
+        return min;
+    }
+    u32 range = max - min + 1;
+    return min + (NextU32() % range);
+}
+
+} // namespace bench
+} // namespace core
