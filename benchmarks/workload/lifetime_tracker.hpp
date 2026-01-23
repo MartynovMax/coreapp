cdeca6a54571ca5aff9507c313458102563aea7d
MartynovMax <m555ax@icloud.com>
2026-01-23 17:00:54 +0100

    Create benchmarks/workload/lifetime_tracker.hpp with public API
    Refs #123

---
+++ benchmarks/workload/lifetime_tracker.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// =============================================================================
+// lifetime_tracker.hpp
+// Tracking of live objects and implementation of lifetime models.
+// =============================================================================
+
+#include "../../core/base/core_types.hpp"
+#include "workload_params.hpp"
+
+namespace core {
+namespace bench {
+
+} // namespace bench
+} // namespace core
