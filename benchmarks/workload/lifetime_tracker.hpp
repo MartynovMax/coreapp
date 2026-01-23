ddb1da865d359ba84e263ca9793ad23a6b8e4f79
MartynovMax <m555ax@icloud.com>
2026-01-23 17:08:32 +0100

    Declare LifetimeTracker class interface
    Refs #123

--- benchmarks/workload/lifetime_tracker.hpp
+++ benchmarks/workload/lifetime_tracker.hpp
@@ -7,6 +7,8 @@
 
 #include "../../core/base/core_types.hpp"
 #include "workload_params.hpp"
+#include "common/seeded_rng.hpp"
+#include "core/memory/core_allocator.hpp"
 
 namespace core {
 namespace bench {
@@ -15,6 +17,43 @@ struct AllocInfo {
     void* ptr;
     u32 size;
     u64 allocTime; // Operation index when allocated
+};
+
+class LifetimeTracker {
+public:
+    LifetimeTracker(LifetimeModel model, u32 maxLiveObjects,
+                    SeededRNG& rng, IAllocator* allocator) noexcept;
+    ~LifetimeTracker() noexcept;
+
+    // Register a new allocation
+    void Track(void* ptr, u32 size, u64 opIndex) noexcept;
+
+    // Select an object to free (according to model)
+    void* SelectForFree() noexcept;
+
+    // Get all live objects
+    void GetAllLive(AllocInfo** outArray, u32* outCount) noexcept;
+
+    // Clear all tracks (bulk reclaim)
+    void Clear() noexcept;
+
+    // Metrics
+    u32 GetLiveCount() const noexcept;
+    u64 GetLiveBytes() const noexcept;
+    u64 GetPeakBytes() const noexcept;
+
+private:
+    LifetimeModel _model;
+    u32 _maxLiveObjects;
+    SeededRNG& _rng;
+    IAllocator* _allocator;
+
+    AllocInfo* _buffer = nullptr;
+    u32 _capacity = 0;
+    u32 _count = 0;
+
+    u64 _totalLiveBytes = 0;
+    u64 _peakLiveBytes = 0;
 };
 
 } // namespace bench
